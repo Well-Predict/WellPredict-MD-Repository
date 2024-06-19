de71d1d0dc01fc51d254df51b5295ea35addfca5
Rifqi Aditya <aditya.rifqi30@gmail.com>
2024-06-19 18:17:12 +0700

    Adding Login & Register Page & Implementing Auth

---
+++ app/src/main/java/com/bangkit/wellpredict/data/api/response/ErrorResponse.kt
@@ -0,0 +1,21 @@
+package com.bangkit.wellpredict.data.api.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ErrorResponse(
+
+	@field:SerializedName("code")
+	val code: String? = null,
+
+	@field:SerializedName("errors")
+	val errors: Errors? = null,
+
+	@field:SerializedName("status")
+	val status: String? = null
+)
+
+data class Errors(
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
